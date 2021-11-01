ec1f0f3b102c59dfa58421d091acdf5eba145b70
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2021-11-01 14:47:49 +0530

    update v1.1
    Two more features added

--- projects/ordering.java
+++ projects/ordering.java
@@ -1,5 +1,12 @@
 /*
-Need to make synchronization to order goods from different fields at a same time
+   Name : Ordering System
+   @author : Tirtha Jyoti Bol
+   Version : 1.0
+   
+   Needs to be done : 
+		1. Need to make synchronization to order goods from different fields at a same time
+   Bugs :
+        1. 
 */
 
 package projects;
@@ -11,6 +18,8 @@ public class ordering {
 	public String name;
 	public String Address;
 	public String Phone;
+	public static boolean cancel;
+	public static int total = 0;
 	
 	    ordering(){	};
 	
@@ -22,7 +31,8 @@ public class ordering {
 		  
 		  void orderPlaced() {
 			  
-			  System.out.println("Hello "+name+" Your Order Has been generated and would be delivered to "+Address+", We will contact you at "+Phone);
+			  System.out.print("Hello "+name+" Your Order Has been generated and would be delivered to "+Address+", We will contact you at "+Phone);
+			  System.out.println(" Total : "+total);
 			  System.out.println("Thank You !");
 		  }
 		  
@@ -43,9 +53,8 @@ public class ordering {
 		String Phone = scan.nextLine();
 		System.out.println("Enter Your Address : ");
 		String Address = scan.nextLine();
-		
-		
 		ordering order = new ordering(name,Address,Phone);
+		
 		
 		// choose 
 		System.out.println("what are you searching for : please select a number  ");
@@ -54,18 +63,39 @@ public class ordering {
 		System.out.println("3. Clothes ");
 		
 		int choose = scan.nextInt();
+		
+		// Food Order
 		if(choose == 1) {
 			FoodOrder food = new FoodOrder();
 			food.menu();
 			if(food.payment == true) {
 				order.orderPlaced();
 			}
+			else {
+				order.orderCancelled();
+			}
 		}
+		
+		// Groceries Order
 		else if(choose == 2) {
-			System.out.println("Groceries Shopping");
+			Groceries gro = new Groceries();
+			gro.list();
+			if(gro.payment == true) {
+				order.orderPlaced();
+			}
+			else {
+				order.orderCancelled();
+			}
 		}
 		else if(choose == 3) {
-			System.out.println("Clothes Shopping ");
+			clothes clo = new clothes();
+			clo.list();
+			if(clo.payment == true) {
+				order.orderPlaced();
+			}
+			else {
+				order.orderCancelled();
+			}
 		}
 		else {
 			System.out.println("Invalid Choice");
@@ -75,8 +105,6 @@ public class ordering {
 	}
 	
 	static class FoodOrder{
-		int price;
-		String order;
 		boolean payment;
 		int pay=0;
 		
@@ -108,7 +136,7 @@ public class ordering {
 				System.out.println("Sorry this Dish is not present in the menu");
 			}
 			
-			System.out.println("Press -1 when your Ordering is Over and press any key if you want to Order more ");
+			System.out.println("Type -1 when your Ordering is Over and press any key if you want to Order more ");
 			int orderOver = scan.nextInt();
 			if(orderOver == -1) {
 				System.out.println("You have a total of Rs."+pay+" to be payed");
@@ -119,7 +147,8 @@ public class ordering {
 					int payOnline = scan.nextInt();
 					if(payOnline == 5) {
 						payment = true;
-						System.out.println("Paid! congratulations your order has been succesfull ");
+						total = pay;
+						System.out.println("Paid! congratulations your order has been succesful ");
 					}
 					else {
 						System.out.println("Payment failed");
@@ -136,8 +165,152 @@ public class ordering {
 			
 			
 		}
-		}
+	}
 		 
 	}
+	
+	
+	static class Groceries{
+		boolean payment;
+		int pay = 0;
+		void list() {
+			System.out.println("Your Grocery item list is shown below, select what you want to purchase");
+			HashMap<String , Integer> list = new HashMap<>();
+			list.put("Rice", 60);
+			list.put("Sugar", 40);
+			list.put("Salt", 20);
+			list.put("Dal", 50);
+			for(Map.Entry<String,Integer> lists : list.entrySet()) {
+				System.out.println("Item : "+lists.getKey() +" ("+lists.getValue()+")");
+			}
+			
+			System.out.println("What you want to order ");
+			
+			while(true) {
+			
+			String ord = scan.next();
+			if(list.containsKey(ord)) {
+				pay = pay + list.get(ord);
+			}
+			else {
+				System.out.println("Item Not found");
+			}
+			
+			System.out.println("Type -1 to Order , Type 0 to cancel and type any other key to order more ");
+			int choose = scan.nextInt();
+			
+			// Customer has finished shopping and has to order
+			if(choose == -1) {
+				
+				System.out.println("You have a total of Rs"+ pay +" to pay Do You Want to pay Online ");
+				String ans = scan.next();
+				if(ans.equalsIgnoreCase("yes")) {
+					System.out.println("Press 5 to pay");
+					int onlinePay = scan.nextInt();
+					if(onlinePay == 5) {
+						payment = true;
+						total = pay;
+						System.out.println("Paid Successfully ");
+					}
+					else {
+						payment = false;
+						System.out.println("Transaction Error ");
+					}
+				}
+				else {
+					payment = true;
+					System.out.println("Your order has been taken into action , you have choosen COD ");
+				}
+				
+				break;
+		  }
+			
+			if(choose == 0) {
+				payment = false;
+				cancel = true;
+				System.out.println("Your Order has been cancelled");
+				break;
+			}
+			
+	   }
+			
+			
+    }
+ }
+	
+	
+	
+	static class clothes{
+		boolean payment;
+		int pay = 0;
+		void list() {
+			System.out.println("Your clothes item list is shown below, select what you want to purchase");
+			HashMap<String , Integer> list = new HashMap<>();
+			list.put("Sports", 1999);
+			list.put("Jeans", 1700);
+			list.put("Shirt", 900);
+			list.put("Joggers", 899);
+			list.put("Shorts", 600);
+			list.put("Underwears", 500);
+			for(Map.Entry<String,Integer> lists : list.entrySet()) {
+				System.out.println("Item : "+lists.getKey() +" ("+lists.getValue()+")");
+			}
+			
+			System.out.println("What you want to order ");
+			
+			while(true) {
+			
+			String ord = scan.next();
+			if(list.containsKey(ord)) {
+				pay = pay + list.get(ord);
+			}
+			else {
+				System.out.println("Item Not found");
+			}
+			
+			System.out.println("Type -1 to Order , Type 0 to cancel and type any other key to order more ");
+			int choose = scan.nextInt();
+			
+			// Customer has finished shopping and has to order
+			if(choose == -1) {
+				
+				System.out.println("You have a total of Rs"+ pay +" to pay Do You Want to pay Online ");
+				String ans = scan.next();
+				if(ans.equalsIgnoreCase("yes")) {
+					System.out.println("Press 5 to pay");
+					int onlinePay = scan.nextInt();
+					if(onlinePay == 5) {
+						payment = true;
+						total = pay;
+						System.out.println("Paid Successfully ");
+					}
+					else {
+						payment = false;
+						System.out.println("Transaction Error ");
+					}
+				}
+				else {
+					payment = true;
+					System.out.println("Your order has been taken into action , you have choosen COD ");
+				}
+				
+				break;
+		  }
+			
+			if(choose == 0) {
+				payment = false;
+				cancel = true;
+				System.out.println("Your Order has been cancelled");
+				break;
+			}
+			
+	   }
+			
+			
+    }
+ }
+	
+	
+	
 
 }
