d943e58c996d332c2aa31d17f3c52f1034ea8220
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2021-12-01 19:10:55 +0530

    Create Implementation using deque.java

---
+++ Queue/Implementation using deque.java
@@ -0,0 +1,40 @@
+package Queue;
+import java.util.*;
+public class Implement_using_DEQUEUE {
+	public static void main(String[]args) {
+		queue q = new queue();
+		q.push(1);
+		q.push(2);
+		q.push(3);
+		q.push(4);
+		
+		q.peek();   // peek should be 1
+		q.print();  // [1,2,3,4]
+		
+		q.poll();
+		q.peek();  // peek should be 2
+		q.print(); // [2,3,4]
+	}
+	
+	static class queue{
+		Deque<Integer>dq = new LinkedList<>();
+		
+		void push(int x) {
+			dq.add(x);
+		}
+		
+		int poll() {
+			if(dq.size()==0) return -1;
+			return dq.poll();
+		}
+		
+		void peek() {
+			System.out.println(dq.peek());
+		}
+		
+		void print() {
+			System.out.println(dq);
+		}
+		
+	}
+}
