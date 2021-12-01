6723ace0b6b284d5ef5290bfa197cb27f434f870
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2021-12-01 19:02:56 +0530

    Create Implement using DEQUEUE.java

---
+++ stack/Implement using DEQUEUE.java
@@ -0,0 +1,49 @@
+/*
+ 	  DEQUE is a "Doubly Ended Queue" - 
+ 	  Here the operations (push , pull , peek) occurs at both side of the Queue
+  */
+
+package stack;
+import java.util.*;
+public class Implement_using_dequeue {
+	public static void main(String[]args) {
+		stack s = new stack();
+		s.push(0);
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		
+		s.peek();   // 3 should be the peek
+		s.print();  
+		
+		s.pull();
+		s.peek(); // 2 should be the peek
+		s.print();
+	}
+	
+  
+  
+	static class stack{
+		Deque<Integer> dq = new LinkedList<>();
+		
+		// For push insert elements at the beginning
+		void push(int x) {
+			dq.offerFirst(x);
+		}
+		
+		// For pull perform poll operations in beginning
+		int pull() {
+			if(dq.size()==0) return -1;
+			return dq.pollFirst();
+		}
+		
+		void peek() {
+			System.out.println(dq.peekFirst());
+		}
+		
+		void print() {
+			System.out.println(dq);
+		}
+		
+	}
+}
