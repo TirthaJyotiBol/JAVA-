1634993b6c8920c371af1f433a96fb2196b17b52
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2021-12-14 21:40:36 +0530

    Update 2.numberOfWaysToReachEndOfMatrix.java

--- recursion/2.numberOfWaysToReachEndOfMatrix.java
+++ recursion/2.numberOfWaysToReachEndOfMatrix.java
@@ -1,49 +1,32 @@
 /*
+Q. Find the number of paths to reach the matrix end
+ 
+ 
  * if we move to the last row / column then we only have one path
  * Hense for getting total paths we need to move to the last -> which is taken as base case 
  * 
  * thus the equation would be == NOW(row-1,cols)+numOfWays(row,cols-1)
- * 
- * */
+ 
+    ALGO:
+     1. If row = 1 || col = 1 -> return 1
+     2. Find for n-1
+     3. Increase Row + Increase Column => Total path
+  
+  */
 
-package recursion;
-import java.util.*;
-public class numberOfWaysToReachEndOfMatrix {
+package Recursion;
+public class Paths_matrix_count {
 	public static void main(String[]args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the size of rows");
-		int row = scan.nextInt();
-		System.out.println("Enter the size of columns");
-		int cols = scan.nextInt();
-		
-		int[][]mat = new int[row][cols];
-		
-		System.out.println("Enter the matrix");
-		for(int i=0;i<row;i++){
-			for(int j=0;j<cols;j++) {
-				mat[i][j]=scan.nextInt();
-			}
-		}
-		
-		int ways = numOfWays(row,cols);
-		
-		System.out.println("There are : "+ways+" ways ");
-		
-		
-		
-		
-		
+		int row = 3;
+		int col = 3;
+		int ans = paths(row,col);
+		System.out.println(ans);
 	}
 	
-	
-	static int numOfWays(int row,int cols) {
+	static int paths(int row,int col) {
+		if(row==1 || col==1) return 1;
 		
-		if(row==1 || cols==1) {return 1;}
-		
-		else return numOfWays(row-1,cols)+numOfWays(row,cols-1);
-		
+		return paths(row,col-1)+paths(row-1,col);
 	}
-
-	
-	
 }
+
