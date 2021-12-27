a7e96b915a488b81098fc0547cbf7a9e6642ef3b
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2021-12-27 21:32:45 +0530

    Update Delete a Node .java

--- BST/Delete a Node .java
+++ BST/Delete a Node .java
@@ -12,33 +12,48 @@ public class Delete_node {
 		root = tree.create(root, 80);
 		root = tree.create(root, 10);
 		
-		deletion(root,30);
 		inorder(root);
+		System.out.println();
+		deletion(root,30);
+		deletion(root,80);
+		inorder(root);
+		
 	}
+	
 	static Node deletion(Node root, int key) {
 		if(root == null) return root;
 		
 		if(key > root.data) {
 			root.right = deletion(root.right,key);
 		}
-		if(key< root.data) {
+		else if(key < root.data) {
 			root.left = deletion(root.left,key);
 		}
 		
 		// Only 1 child
 	else {
+		
+		// If both of the childern are null i.e. Leaf
+		if(root.left == null && root.right == null)
+				return null;
+		// If either of then children is null
 		if(root.left == null)
-			return root.right;
-		else if(root.right == null)
-			return root.left;
+				return root.right;
+		if(root.right == null)
+				return root.left;
 		
 		// If having 2 children then take the minimum from the right and replace it with that element
-		
+		if(root.left != null && root.right != null) {
 		root.data = min(root.right);
 		root.right = deletion(root.right,root.data);
 		}
+		
+	}
+		
 		return root;
 	}
+	
+	// Code to find the minimum element
 	static int min(Node root) {
 		
 		int min = root.data;
@@ -56,4 +71,5 @@ public class Delete_node {
 		System.out.print(root.data+" ");
 		inorder(root.right);
 	}
+	
 }
